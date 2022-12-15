dc946104331986bfca039d7ad829f11a30afdc23
Vishal Rawani <115313503+Vishalvermaimv@users.noreply.github.com>
2022-12-15 17:06:04 +0530

    Add files via upload

---
+++ QuizQuestions.py
@@ -0,0 +1,12 @@
+question_prompt =[
+    "which function overloads the( + )operator? \n (a)__add__() (b)__plus__() (c)__sum__() (d)none" ,
+    "which operator is overloaded by __ivert__()? \n (a) ~ (b) ! (c) ^ (d) - ",
+    "which function overloads the == operator? \n (a)__eq__()__equ__ (c)__isequal__() (d)None" ,
+    "which operator is overloaded by __lg__()? \n (a) < (b) > (c) != (d) None" ,
+    "which function overloads the >> operator? \n (a)__more__() (b)__gt__() (c)__ge__()(d)None" ,
+    "which function overloads the // operator? \n (a)__div__()(b)__ceildiv__()(c)__floordiv__()(d)__truediv__()" ,
+    "which modules in python support regular expression? \n(a)re (b)regex (c)json (d) None" ,
+    "[0-9] character class will match all the single digit number? \n (a) True (b) False" ,
+    "( ? ) in RegX means either once or not at all? \n (a) True (b) False" ,
+    "what is python? \n (a) dynamic langauge (b) static langauge"
+]
